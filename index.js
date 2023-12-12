51104c37310fc4cbe5562928dce3a6a4c29e985a
nzlongboi <116671677+nzlongboi@users.noreply.github.com>
2023-12-12 16:53:24 +1300

    Add event listners to playing squares. X and O show up on click and do not repopulate playing square on click.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const playingBoard = document.getElementById('game-board');
+const displayCurrentPlayer = document.getElementById('current-player');
+let currentPlayer = 1;
+
+
+
 
 /*gameboard object*/
 
@@ -10,7 +15,24 @@ const gameBoardObject = (function() {
         const playingSpace = document.createElement('div');
         playingSpace.style.border = '1px solid black';
         playingSpace.style.boxSizing = 'border-box';
+        playingSpace.style.textAlign = 'center';
+        playingSpace.style.fontSize = '140px';
+        playingSpace.onclick = () => {
+            if (playingSpace.innerText == 'X' || playingSpace.innerText == 'O') {
+                alert('This space is already taken.');
+                return;
+            }
+            playingSpace.innerText = 'X';
+            if (displayCurrentPlayer.innerText == '1') {
+                displayCurrentPlayer.innerText = '2';
+            } else if (displayCurrentPlayer.innerText == '2') {
+                displayCurrentPlayer.innerText = '1';
+            }
+            if (displayCurrentPlayer.innerText =='2') {
+                playingSpace.innerText = 'O';
+            }
 
+        }
         playingBoard.appendChild(playingSpace);
     }
 
@@ -27,7 +49,7 @@ const player2 = (function() {
 
 
 const gameLogic = (function() {
-    
+
 
 
 })();
