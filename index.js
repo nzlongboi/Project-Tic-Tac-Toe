f46f1f3ae9435b88872dece0d6eef58dff758800
nzlongboi <116671677+nzlongboi@users.noreply.github.com>
2023-12-18 15:06:48 +1300

    Finish making and implementing win indicator. Wins are now recognised and displayed. To be further updated.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const playingBoard = document.getElementById('game-board');
 const displayCurrentPlayer = document.getElementById('current-player');
+const playingSpace1 = document.getElementById('playing-space1');
+const playingSpace2 = document.getElementById('playing-space2');
+const playingSpace3 = document.getElementById('playing-space3');
+const playingSpace4 = document.getElementById('playing-space4');
+const playingSpace5 = document.getElementById('playing-space5');
+const playingSpace6 = document.getElementById('playing-space6');
+const playingSpace7 = document.getElementById('playing-space7');
+const playingSpace8 = document.getElementById('playing-space8');
+const playingSpace9 = document.getElementById('playing-space9');
 let currentPlayer = 1;
 
 
@@ -8,7 +17,6 @@ let currentPlayer = 1;
 /*gameboard object*/
 
 const gameBoardObject = (function() {
-    let gameBoardArray = [];
     const playingSpace = document.querySelectorAll('.playing-space');
     
     playingSpace.forEach(space => {
@@ -28,25 +36,41 @@ const gameBoardObject = (function() {
             
 
             displayCurrentPlayer.innerText = currentPlayer === '1' ? '2' : '1';
+            
+            setTimeout(() => {
+                checkWinCondition();
+            }, 0);    
         };
 
+        
         space.style.fontSize = '140px';
         space.style.textAlign = 'center';
     });
-})();
 
-const player1 = (function() {
-    const playerToken = 'X';
-})();
+    
+    function checkWinCondition() {
+        const winConditions = [
+            [playingSpace1, playingSpace2, playingSpace3],
+            [playingSpace4, playingSpace5, playingSpace6],
+            [playingSpace7, playingSpace8, playingSpace9],
+            [playingSpace1, playingSpace4, playingSpace7],
+            [playingSpace2, playingSpace5, playingSpace8],
+            [playingSpace3, playingSpace6, playingSpace9],
+            [playingSpace1, playingSpace5, playingSpace9],
+            [playingSpace3, playingSpace5, playingSpace7]
+        ];
 
+        for (const condition of winConditions) {
+            const [a, b, c] = condition;
 
-const player2 = (function() {
-    const playerToken = 'O';
-})();
-
-
-const gameLogic = (function() {
-
-
+            if (a.innerText === 'X' && b.innerText === 'X' && c.innerText === 'X') {
+                alert('Player 1 wins');
+                return;
+            } else if (a.innerText === 'O' && b.innerText === 'O' && c.innerText === 'O') {
+                alert('Player 2 wins');
+                return;
+            }
+        }
+    }
 
 })();
